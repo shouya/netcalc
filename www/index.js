3fbbf88aeab6ffe8f542294c707537cdb5939f6e
Shou Ya <shouya@users.noreply.github.com>
2021-07-05 21:52:04 +0800

    tweak the interface

--- www/index.js
+++ www/index.js
@@ -2,7 +2,12 @@ import * as wasm from "netcalc";
 
 window.conv = function () {
   const input = document.querySelector("#rules textarea").value;
-  document.querySelector("#results textarea").value = wasm.convert("\n", input);
+  let sep = document.querySelector("#separator").value;
+  if (sep == "\\n") {
+    sep = "\n";
+  }
+  const output = wasm.convert(sep, input);
+  document.querySelector("#results textarea").value = output;
 };
 
-document.onready = window.conv;
+window.conv();
