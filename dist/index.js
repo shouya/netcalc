8d5661d0719e245aa23d2b817d2b1f3a20e45861
Shou Ya <shouya@users.noreply.github.com>
2023-07-13 00:38:57 +0900

    Updates

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,12 @@
-import * as wasm_bindgen from "./netcalc.js";
+import { convert } from "./netcalc.js";
 
 async function conv() {
-  await wasm_bindgen();
-
   const input = document.querySelector("#rules textarea").value;
   let sep = document.querySelector("#separator").value;
   if (sep == "\\n") {
     sep = "\n";
   }
-  const output = wasm_bindgen.convert(sep, input);
+  const output = convert(sep, input);
   document.querySelector("#results textarea").value = output;
 }
 
