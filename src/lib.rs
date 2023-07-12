5093cbddead7550c90518dda03acfb4335be1f27
Shou Ya <shouya@users.noreply.github.com>
2023-07-12 23:24:45 +0900

    minimize wasm file size

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 mod netcalc;
-mod utils;
 
 use wasm_bindgen::prelude::*;
 
-#[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub fn convert(sep: &str, s: &str) -> String {
+  console_error_panic_hook::set_once();
   netcalc::convert("4", sep, s).unwrap_or_else(|err| format!("{}", err))
 }
