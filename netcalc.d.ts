bbce2660a18adc53912aaa752012a42456fd80ee
Shou Ya <shouya@users.noreply.github.com>
2023-07-13 00:17:01 +0900

    Updates

--- netcalc.d.ts
+++ netcalc.d.ts
@@ -1,8 +1,32 @@
-/* tslint:disable */
-/* eslint-disable */
+declare namespace wasm_bindgen {
+	/* tslint:disable */
+	/* eslint-disable */
+	/**
+	* @param {string} sep
+	* @param {string} s
+	* @returns {string}
+	*/
+	export function convert(sep: string, s: string): string;
+	
+}
+
+declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+declare interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly convert: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+}
+
 /**
-* @param {string} sep
-* @param {string} s
-* @returns {string}
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
 */
-export function convert(sep: string, s: string): string;
+declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
