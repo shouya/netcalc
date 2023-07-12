c02b1021d0743271c5a20b3dae88d4810ca5a9e7
Shou Ya <shouya@users.noreply.github.com>
2023-07-13 01:14:27 +0900

    Updates

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
-import { convert } from "./netcalc.js";
+const { convert } = wasm_bindgen;
 
 async function conv() {
+  await wasm_bindgen("./netcalc_bg.wasm");
+
   const input = document.querySelector("#rules textarea").value;
   let sep = document.querySelector("#separator").value;
   if (sep == "\\n") {
--- dist/netcalc.d.ts
+++ dist/netcalc.d.ts
@@ -1,15 +1,18 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {string} sep
-* @param {string} s
-* @returns {string}
-*/
-export function convert(sep: string, s: string): string;
+declare namespace wasm_bindgen {
+	/* tslint:disable */
+	/* eslint-disable */
+	/**
+	* @param {string} sep
+	* @param {string} s
+	* @returns {string}
+	*/
+	export function convert(sep: string, s: string): string;
+	
+}
 
-export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+declare type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
 
-export interface InitOutput {
+declare interface InitOutput {
   readonly memory: WebAssembly.Memory;
   readonly convert: (a: number, b: number, c: number, d: number, e: number) => void;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
@@ -17,17 +20,6 @@ export interface InitOutput {
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_free: (a: number, b: number, c: number) => void;
 }
-
-export type SyncInitInput = BufferSource | WebAssembly.Module;
-/**
-* Instantiates the given `module`, which can either be bytes or
-* a precompiled `WebAssembly.Module`.
-*
-* @param {SyncInitInput} module
-*
-* @returns {InitOutput}
-*/
-export function initSync(module: SyncInitInput): InitOutput;
 
 /**
 * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
@@ -37,4 +29,4 @@ export function initSync(module: SyncInitInput): InitOutput;
 *
 * @returns {Promise<InitOutput>}
 */
-export default function __wbg_init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
+declare function wasm_bindgen (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
