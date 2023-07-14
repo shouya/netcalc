b4b93f80e4d666b0e770e6a0826a0833849494ca
Shou Ya <shouya@users.noreply.github.com>
2023-07-14 14:59:46 +0900

    allow selecting ipv6 on the front-end

--- www/index.js
+++ www/index.js
@@ -4,11 +4,13 @@ async function conv() {
   await wasm_bindgen("./netcalc_bg.wasm");
 
   const input = document.querySelector("#rules textarea").value;
+  let ver = document.querySelector("#version").value;
+
   let sep = document.querySelector("#separator").value;
   if (sep == "\\n") {
     sep = "\n";
   }
-  const output = convert("v4", sep, input);
+  const output = convert(ver, sep, input);
   document.querySelector("#results textarea").value = output;
 }
 
